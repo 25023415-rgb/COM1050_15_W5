61945a0672eea557c12bf1017829c857bf12b47c
Trọng Nguyễn <25023415@vnu.edu.vn>
2025-12-07 11:30:42 +0700

    Add files via upload

---
+++ w5a1.py
@@ -0,0 +1,7 @@
+a, b = map(int, input())
+def Max_Of_Two(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+print(Max_Of_Two(a, b))
---
+++ w5a2.py
@@ -0,0 +1,4 @@
+a, b = map(int, input().split())
+def swap(a, b):
+    return tuple([b, a])
+print(swap(a, b))
